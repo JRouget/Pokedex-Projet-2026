9a0bc3713f373083c47913ede5eb7471ff0eaf58
EthanCarteron <ethan.carteron@gmail.com>
2026-01-20 10:54:55 +0100

    feat: meilleure segmatation des fonctions et retour des infos

---
+++ service/pokAPI.ts
@@ -0,0 +1,11 @@
+import type {Pokemon} from "../type/poke-type";
+
+export async function pokeApiFetcher(identificator: string | number) : Promise<Pokemon | null> {
+    try {
+        const reponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${identificator}`);
+        return await reponse.json() as Pokemon;
+    } catch (error) {
+        console.error(error);
+        return null;
+    }
+}
