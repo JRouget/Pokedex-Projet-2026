a464c93a536e1d7a93b744b1a09a989b5cd38023
Jrouget <rougetjoshua@gmail.com>
2026-01-20 10:33:34 +0100

    feat : added new files

---
+++ type/poke-type.ts
@@ -0,0 +1,67 @@
+javascript 
+export interface APINamedRessource{
+    name: string;
+    url: string;
+}
+
+export interface APIListResponse{
+    count: number;
+    previous: string | null;
+    next: string | null;
+
+    results: APINamedRessource[];
+}
+
+export interface PokemonImage {
+    front_default: string;
+    other?: {
+        "official-artwork":{
+            front_default: string;
+        };
+        home?:{
+            front_default: string;
+        }
+    };
+    showdown?: {
+        front_default: string;
+    };
+}
+
+export interface PokemonType {
+    slot: number;
+    type: APINamedRessource;
+}
+
+export interface PokemonStat {
+    base_stat: number;
+    stat: APINamedRessource;
+}
+
+export interface PokemonAbility {
+    is_hidden: boolean;
+    slot: number;
+    ability: APINamedRessource;
+}
+
+export interface PokemonCries {
+    latest: string;
+    legacy: string;
+}
+
+export interface PokemonMove {
+    move: APINamedRessource;
+}
+
+export interface Pokemon {
+    id: number;
+    name: string;
+    height: number;
+    weight: number;
+
+    image: PokemonImage;
+    types: PokemonType[];
+    stats: PokemonStat[];
+    abilities: PokemonAbility[];
+    moves: PokemonMove[];
+    cries: PokemonCries;
+}
