1580fe914495bf42bdc04479568b544e403add6e
EthanCarteron <ethan.carteron@gmail.com>
2026-01-20 11:52:21 +0100

    feat: premier affichage

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,27 @@
-import { pokeLiteApiFetcher, fetchFiltersList, fetchPokemonByFilter } from './services/details-api'
+import { getListPokemons } from './service/specifyAPI'
 
 type LitePokemon = { name: string; url: string; };
-type SortMode = 'id' | 'name';
-type OrderMode = 'asc' | 'desc';
 
 let fullRepository: LitePokemon[] = [];
-let filteredRepository: LitePokemon[] = [];
 let currentDisplayList: LitePokemon[] = [];
 
+async function affichage () {
+    try {
+        const response = await getListPokemons();
+
+        if (response && response.results) {
+            fullRepository = response.results;
+            currentDisplayList = [...fullRepository];
+
+            console.log("Voici la liste brute :", currentDisplayList);
+
+            if (currentDisplayList.length > 0) {
+                console.log(`Le premier Pokémon est : ${currentDisplayList[0].name}`);
+            }
+        }
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+affichage();
